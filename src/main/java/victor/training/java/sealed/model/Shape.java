130182b12d4da3123228228883aa4a1a89ea0208
Victor Rentea <victorrentea@gmail.com>
2023-02-06 14:30:34 +0200

    Visitor in actiob

--- src/main/java/victor/training/java/sealed/model/Shape.java
+++ src/main/java/victor/training/java/sealed/model/Shape.java
@@ -4,4 +4,5 @@ package victor.training.java.sealed.model;
 import victor.training.java.sealed.ShapeVisitor;
 
 public interface Shape  {
+  void accept(ShapeVisitor visitor);
 }
--- src/main/java/victor/training/java/sealed/model/Square.java
+++ src/main/java/victor/training/java/sealed/model/Square.java
@@ -4,7 +4,10 @@ import victor.training.java.sealed.ShapeVisitor;
 
 public final class Square implements Shape {
 	private final int edge;
-
+	@Override
+	public void accept(ShapeVisitor visitor) {
+		visitor.visit(this); //wtf !?!?!
+	}
 	public Square(int edge) {
 		this.edge = edge;
 	}
