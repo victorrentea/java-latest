9bf5bd8840eb990f5187b4adb49c8029708038a1
Victor Rentea <victorrentea@gmail.com>
2023-02-06 10:50:21 +0200

    Java 11 immutable (ev + Lobmook @Value)

--- src/main/java/victor/training/java/records/Immutable.java
+++ src/main/java/victor/training/java/records/Immutable.java
@@ -2,21 +2,40 @@ package victor.training.java.records;
 
 import com.google.common.collect.ImmutableList;
 import jakarta.persistence.Entity;
+import jdk.jfr.DataAmount;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+//@Data
+//class PotSiEu {
+//  private String a,b,c;
+//}
+//@Slf4j
+// ==> Lombok la putere pt ca Java e ffff verbose
+//@Data (hate) = getter+SETTER🤮+tostring+hashcode+equals
+//@Value (❤️) = toate campurile finale+ctor+getter+hashcode+equals+tostring
+//class ImmutableLombok {
+//  String name;
+//  Other other;
+//  List<Integer> list;
+//}
+
+
 public class Immutable {
   private final String name;
   private final Other other;
-  private final ImmutableList<Integer> list;
+//  private final ImmutableList<Integer> list;
+  private final List<Integer> list;
 
-  public Immutable(String name, Other other, ImmutableList<Integer> list) {
+//  public Immutable(String name, Other other, ImmutableList<Integer> list) {
+  public Immutable(String name, Other other, List<Integer> list) {
     this.name = name;
     this.other = other;
-    this.list = list;
+    this.list = List.copyOf(list);
   }
 
   public String getName() {
@@ -27,20 +46,22 @@ public class Immutable {
     return other;
   }
 
-  // ** 1
+  // ** 1 new ArrayList
 //  public List<Integer> getList() {
 //    return new ArrayList<>(list);
 //    // 1) ineficient cu mem - malloc;
 //    // 2) minte apelantu : el poate crede ca a sters lista.
 //  }
 
-  // ** 2:
-  public ImmutableList<Integer> getList() {
+  // ** 2: Collections.unmodifiableList
+//  public ImmutableList<Integer> getList() {
+//    return list;
+//  }
+  // ** 3: Guava lib : ImmutableList
+  public List<Integer> getList() {
     return list;
   }
-  // ** 3: Guava lib : ImmutableList
-
-
+// ** 4: java 10: List.copyOf sau .of
   @Override
   public String toString() {
     return "Immutable{" +
--- src/main/java/victor/training/java/records/ImmutablePlay.java
+++ src/main/java/victor/training/java/records/ImmutablePlay.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
