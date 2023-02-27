8d26e9bc339e7568017cf8efa2cf61dd4cb1352b
Victor Rentea <victorrentea@gmail.com>
2023-02-27 12:02:31 +0200

    convertit clasa imutabila --> record

--- src/main/java/victor/training/java/records/RecordLab.java
+++ src/main/java/victor/training/java/records/RecordLab.java
@@ -1,28 +1,17 @@
 package victor.training.java.records;
 
-class Dot {
-  private final int x;
-  private final int y;
 
-  Dot(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  public int getX() {
-    return x;
-  }
-
-  public int getY() {
-    return y;
-  }
-}
+record Dot(int x, int y) {}
+// autogenereaza : constructor, getter (fara "get-"), toString, hashCode/equals ~= @Value
 
 public class RecordLab {
   public static void main(String[] args) {
     Dot dot = new Dot(1, 2);
 
-    System.out.println(dot.getX());
+    System.out.println(dot.x());
+    System.out.println(dot);
+
+    System.out.println(new Dot(1, 2).equals(dot));
 
     // TODO la ce sa avem grija inainte sa urmam sfatul intelliJ sa "convert to record" ?
     // - in loc de getX() o sa apara x() => dar n-ar trebui sa-i pese nimanui de cum numesti getterii
