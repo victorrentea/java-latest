86262cb143b615a69b3b6d9e1a6439c6728a2b36
Victor Rentea <victorrentea@gmail.com>
2023-02-27 11:34:50 +0200

    java 17 ftw =  record!

--- src/main/java/victor/training/java/records/Other.java
+++ src/main/java/victor/training/java/records/Other.java
@@ -7,42 +7,12 @@ package victor.training.java.records;
 //  private final String data;
 //}
 
-//@Value // = toate campurile "private final" +
-//// getter toString hashCode/equals constructor
+//@Value // = toate campurile "private final" + getter toString hashCode/equals constructor
 //public class Other {
 //  String data;
 //}
 
-import java.util.Objects;
-
-public final class Other {
-  private final String data;
-
-  public Other(String data) {
-    this.data = data;
-  }
-
-  public String getData() {
-    return this.data;
-  }
-
-  @Override
-  public String toString() {
-    return "Other{" +
-           "data='" + data + '\'' +
-           '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Other other = (Other) o;
-    return Objects.equals(data, other.data);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(data);
-  }
+public record Other(String data) {
 }
+
+// case class din Scala, data class din kt, php8, C#, typescript
