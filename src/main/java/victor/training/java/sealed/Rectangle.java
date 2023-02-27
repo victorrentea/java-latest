f9cc2bd4bde7393bb846d7a70593ca81f26d001b
Victor Rentea <victorrentea@gmail.com>
2023-02-27 16:03:42 +0200

    instanceof

---
+++ src/main/java/victor/training/java/sealed/Rectangle.java
@@ -0,0 +1,4 @@
+package victor.training.java.sealed;
+
+public record Rectangle(int w, int h) implements Shape {
+}
--- src/main/java/victor/training/java/sealed/VisitorPlay.java
+++ src/main/java/victor/training/java/sealed/VisitorPlay.java
@@ -11,9 +11,27 @@ public class VisitorPlay {
                 new Square(10), // 4 * E
                 new Circle(5), // 2 * PI * R
                 new Square(5),
-                new Square(1));
+                new Square(1),
+                new Rectangle(4,3));
 
         // TASK : compute the total perimeter
+        double totalPerimeter = 0;
+
+        // ### instanceof
+        for (Shape shape : shapes) {
+            if (shape instanceof Square) {
+                Square sq = (Square) shape;
+                totalPerimeter += sq.edge()*4;
+            } else if (shape instanceof Circle) {
+                Circle c = (Circle) shape;
+                totalPerimeter += c.radius()*2 * Math.PI;
+            } else {
+                throw new IllegalArgumentException("Unsupported type: " +shape.getClass().getSimpleName());
+            }
+            // PROBLEMA: daca apare maine si Dreptungiul
+        }
+
+        System.out.println("Total perimeter = " + totalPerimeter);
 
         // ## OOP
         // ## VISITOR
