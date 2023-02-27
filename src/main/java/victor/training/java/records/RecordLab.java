59ca558ad817e85afd934a7333871f856cd595a7
Victor Rentea <victorrentea@gmail.com>
2023-02-27 11:59:15 +0200

    immutable Dot

--- src/main/java/victor/training/java/records/RecordLab.java
+++ src/main/java/victor/training/java/records/RecordLab.java
@@ -1,33 +1,28 @@
 package victor.training.java.records;
 
 class Dot {
-  private int x;
-  private int y;
+  private final int x;
+  private final int y;
+
+  Dot(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
 
   public int getX() {
     return x;
   }
 
-  public Dot setX(int x) {
-    this.x = x;
-    return this;
-  }
-
   public int getY() {
     return y;
-  }
-
-  public Dot setY(int y) {
-    this.y = y;
-    return this;
   }
 }
 
 public class RecordLab {
   public static void main(String[] args) {
-    Dot dot = new Dot();
+    Dot dot = new Dot(1, 2);
 
-
+    System.out.println(dot.getX());
 
     // TODO la ce sa avem grija inainte sa urmam sfatul intelliJ sa "convert to record" ?
     // - in loc de getX() o sa apara x() => dar n-ar trebui sa-i pese nimanui de cum numesti getterii
