a87f87d76da0152d1edb671c6c56b7dc32ca2976
Victor Rentea <victorrentea@gmail.com>
2023-02-28 15:48:37 +0200

    daca unul din subtaskuri crapa => celalalt e cancelat (dovada ca join a terminat imediat), iar resultNow imi da o exceptie inapoi:  a crapat subtaskul 1.

--- src/main/java/victor/training/java/loom/BookingApi.java
+++ src/main/java/victor/training/java/loom/BookingApi.java
@@ -17,17 +17,21 @@ public class BookingApi {
   RestTemplate rest = new RestTemplate();
   @GetMapping("booking")
   public String booking() throws ExecutionException, InterruptedException, IOException {
-
+String result;
     try (ShutdownOnFailure scope = new ShutdownOnFailure()) { // daca oricare sub task crapa, intrerupele pe celelalte si termina cu exceptie
       // forkez executia : startez mai multe sub taskuri
       Future<String> futureOffer1 = scope.fork(() -> rest.getForObject("http://localhost:9999/booking-provider-1", String.class));
       Future<String> futureOffer2 = scope.fork(() -> rest.getForObject("http://localhost:9999/booking-provider-2", String.class));
 
-      scope.join();
+      scope.join(); // join a iesit pentru ca provider-1 a dat 500 iar provider-2 a fost intrerupt din apel
       // aici toate future-urile sunt terminate (cu succes sau exceptii)
 
-      return futureOffer1.resultNow() + " + " + futureOffer2.resultNow();
+      result=  futureOffer1.resultNow() + " + " + futureOffer2.resultNow();
     } // .close() se cheama automat
+
+//    System.out.println("Logica cu "+result);
+
+    return result;
   }
 
 }
