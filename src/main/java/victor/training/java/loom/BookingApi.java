5408cc6b6ba2a191bb563a936fa5634e229a50ef
Victor Rentea <victorrentea@gmail.com>
2023-02-28 15:31:17 +0200

    classic future-based concurrency

--- src/main/java/victor/training/java/loom/BookingApi.java
+++ src/main/java/victor/training/java/loom/BookingApi.java
@@ -5,15 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.concurrent.*;
 
 @RestController
 public class BookingApi {
+  private static final ExecutorService threadPool = Executors.newFixedThreadPool(20);
   @GetMapping("booking")
-  public BookingOffersDto booking() {
-//    RestTemplate rest = new RestTemplate();
-//    String bookingOffer1 = rest.getForObject("http://localhost:9999/booking-provider-1", String.class);
-//    String bookingOffer2 = rest.getForObject("http://localhost:9999/booking-provider-1", String.class);
-    return null;
+  public String booking() throws ExecutionException, InterruptedException {
+    RestTemplate rest = new RestTemplate();
+
+    Future<String> futureOffer1 = threadPool.submit(() ->
+            rest.getForObject("http://localhost:9999/booking-provider-1", String.class));
+
+    Future<String> futureOffer2 = threadPool.submit(()->
+            rest.getForObject("http://localhost:9999/booking-provider-2", String.class));
+
+    return futureOffer1.get() + " + " + futureOffer2.get();
   }
 
 }
