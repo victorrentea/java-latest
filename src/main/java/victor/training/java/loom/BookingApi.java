13a030fa07cdbae68407ff32f1f315ed5c84c63d
Victor Rentea <victorrentea@gmail.com>
2023-02-28 15:55:16 +0200

    Inapoi pe happy case

--- src/main/java/victor/training/java/loom/BookingApi.java
+++ src/main/java/victor/training/java/loom/BookingApi.java
@@ -17,19 +17,21 @@ public class BookingApi {
   RestTemplate rest = new RestTemplate();
   @GetMapping("booking")
   public String booking() throws ExecutionException, InterruptedException, IOException {
-String result;
+    String result;
     try (ShutdownOnFailure scope = new ShutdownOnFailure()) { // daca oricare sub task crapa, intrerupele pe celelalte si termina cu exceptie
       // forkez executia : startez mai multe sub taskuri
       Future<String> futureOffer1 = scope.fork(() -> rest.getForObject("http://localhost:9999/booking-provider-1", String.class));
       Future<String> futureOffer2 = scope.fork(() -> rest.getForObject("http://localhost:9999/booking-provider-2", String.class));
+      // aici req http deja s-au dus (sau pleaca fff curand din backgroud)
+      System.out.println("Eventual ceva munca ce CPU ce pot sa o fac fara datele alea");
 
       scope.join(); // join a iesit pentru ca provider-1 a dat 500 iar provider-2 a fost intrerupt din apel
       // aici toate future-urile sunt terminate (cu succes sau exceptii)
 
-      result=  futureOffer1.resultNow() + " + " + futureOffer2.resultNow();
+      result =  futureOffer1.resultNow() + " + " + futureOffer2.resultNow();
     } // .close() se cheama automat
 
-//    System.out.println("Logica cu "+result);
+    System.out.println("Logica cu "+result);
 
     return result;
   }
