f03fe21b972bc8c2ca5bfbb1d6e20aa28198be52
Victor Rentea <victorrentea@gmail.com>
2023-02-28 15:43:13 +0200

    structured concurrency take1

--- src/main/java/victor/training/java/loom/BookingApi.java
+++ src/main/java/victor/training/java/loom/BookingApi.java
@@ -1,26 +1,33 @@
 package victor.training.java.loom;
 
+import jdk.incubator.concurrent.StructuredTaskScope.ShutdownOnFailure;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.List;
 import java.util.concurrent.*;
 
 @RestController
 public class BookingApi {
   private static final ExecutorService threadPool = Executors.newFixedThreadPool(20);
+  RestTemplate rest = new RestTemplate();
   @GetMapping("booking")
-  public String booking() throws ExecutionException, InterruptedException {
-    RestTemplate rest = new RestTemplate();
+  public String booking() throws ExecutionException, InterruptedException, IOException {
 
-    Future<String> futureOffer1 = threadPool.submit(() ->
-            rest.getForObject("http://localhost:9999/booking-provider-1", String.class));
+    try (ShutdownOnFailure scope = new ShutdownOnFailure()) { // daca oricare sub task crapa, intrerupele pe celelalte si termina cu exceptie
+      // forkez executia : startez mai multe sub taskuri
+      Future<String> futureOffer1 = scope.fork(() -> rest.getForObject("http://localhost:9999/booking-provider-1", String.class));
+      Future<String> futureOffer2 = scope.fork(() -> rest.getForObject("http://localhost:9999/booking-provider-2", String.class));
 
-    Future<String> futureOffer2 = threadPool.submit(()->
-            rest.getForObject("http://localhost:9999/booking-provider-2", String.class));
+      scope.join();
+      // aici toate future-urile sunt terminate (cu succes sau exceptii)
 
-    return futureOffer1.get() + " + " + futureOffer2.get();
+      return futureOffer1.resultNow() + " + " + futureOffer2.resultNow();
+    } // .close() se cheama automat
   }
 
 }
