a164ab84a69c1917c2b840b5575964ba92efdfe8
Victor Rentea <victorrentea@gmail.com>
2023-06-21 09:28:54 +0300

    yolo

--- src/main/java/victor/training/java/ImmutableCollections.java
+++ src/main/java/victor/training/java/ImmutableCollections.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class ToList {
+public class ImmutableCollections {
 
 
   public static void main(String[] args) throws IOException {
