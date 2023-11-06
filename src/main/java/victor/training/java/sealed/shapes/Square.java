66f302a37323ff185e5596e0d9e28c2f40afc896
Victor Rentea <victorrentea@gmail.com>
2023-11-06 15:09:07 +0200

    #1 OOP

--- src/main/java/victor/training/java/sealed/shapes/Square.java
+++ src/main/java/victor/training/java/sealed/shapes/Square.java
@@ -1,4 +1,9 @@
 package victor.training.java.sealed.shapes;
 
 public record Square(int edge) implements Shape {
+  @Override
+  public double perimeter() {
+    // #1 OOP logica IN clasa
+    return 4 * edge;
+  }
 }
--- src/main/java/victor/training/java/sealed/shapes/VisitorPlay.java
+++ src/main/java/victor/training/java/sealed/shapes/VisitorPlay.java
@@ -13,12 +13,23 @@ public class VisitorPlay {
                 new Square(5),
                 new Square(1));
 
+        // #1 OOP sa vezi clasele ca Obiect, sa le permiti sa aiba behavior
+//        for (Shape shape : shapes) {
+//            totalPerimeter += shape.perimeter(); -> stream.sum()
+//            list.add(e); -> stream.toList()
+//        }
+        double totalPerimeter = shapes.stream().mapToDouble(Shape::perimeter).sum();
+      // sa vezi clasele doar ca Date si sa pui pe altcineva sa aplice formula corecta
+
+
+
         // TASK : compute the total perimeter
 
         // ## OOP
         // ## VISITOR
         // ## instanceOf
         // ## switch+sealed
+        System.out.println(totalPerimeter);
     }
 }
 
