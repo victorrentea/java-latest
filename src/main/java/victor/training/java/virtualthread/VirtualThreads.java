8c373e78b00baaf4825d839aed7655e026436e4c
Victor Rentea <victorrentea@gmail.com>
2023-11-20 16:44:39 +0200

    CF equivalent of scope.

--- src/main/java/victor/training/java/virtualthread/VirtualThreads.java
+++ src/main/java/victor/training/java/virtualthread/VirtualThreads.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.StructuredTaskScope.ShutdownOnFailure;
 import java.util.concurrent.StructuredTaskScope.Subtask;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -31,10 +33,17 @@ public class VirtualThreads {
     return beer;
   }
 
-  record UserPreferences(String favoriteBeerType, boolean iceInVodka) {}
-  record Beer(String type) {}
-  record Vodka(String type) {  }
-  record DillyDilly(Beer beer, Vodka vodka) {}
+  record UserPreferences(String favoriteBeerType, boolean iceInVodka) {
+  }
+
+  record Beer(String type) {
+  }
+
+  record Vodka(String type) {
+  }
+
+  record DillyDilly(Beer beer, Vodka vodka) {
+  }
 
   @GetMapping("/drink")
   public DillyDilly drink() throws InterruptedException, ExecutionException {
@@ -43,19 +52,39 @@ public class VirtualThreads {
 
     try (ShutdownOnFailure scope = new ShutdownOnFailure()) {
       Subtask<Beer> beerTask = scope.fork(() ->
-          rest.getForObject("http://localhost:9999/api/beer/blond" , Beer.class)); // spawns +1 child virtual thread
+          rest.getForObject("http://localhost:9999/api/beer/blond", Beer.class)); // spawns +1 child virtual thread
 
       Subtask<Vodka> vodkaTask = scope.fork(() ->
           rest.getForObject("http://localhost:9999/vodka", Vodka.class)); // spawns +1 child virtual thread
 
-      scope.join() // Blocks the parent virtual thread until all subtasks complete
-        .throwIfFailed(); // throw exception if any subtasks completed in error
+      scope.join() // Blocks the parent virtual thread until all child subtasks complete
+          .throwIfFailed(); // throw exception if any subtasks completed in error
 
       // Combine subtasks' results
       DillyDilly dilly = new DillyDilly(beerTask.get(), vodkaTask.get());
       log.info("Request completed in {} ms", currentTimeMillis() - t0);
       return dilly;
     }
+  }
+
+
+  // Scope equivalent with Completable Future (until Java 25 LTS)
+  @GetMapping("/drink-cf")
+  public DillyDilly drinkCF() throws InterruptedException, ExecutionException {
+    long t0 = currentTimeMillis();
+
+    CompletableFuture<Beer> beerCF = CompletableFuture.supplyAsync(() ->
+            rest.getForObject("http://localhost:9999/api/beer/blond", Beer.class),
+        Executors.newVirtualThreadPerTaskExecutor());
+
+    CompletableFuture<Vodka> vodkaCF = CompletableFuture.supplyAsync(() ->
+            rest.getForObject("http://localhost:9999/api/vodka", Vodka.class),
+        Executors.newVirtualThreadPerTaskExecutor());
+
+    DillyDilly dilly = beerCF.thenCombine(vodkaCF, DillyDilly::new)
+        .get(); // no problem, since I'm blocking a virtual thread
+    log.info("Request completed in {} ms", currentTimeMillis() - t0);
+    return dilly;
   }
 
 }
