12525c1cd9fd003b2d0753f83be238a6dde94c1e
Victor Rentea <victorrentea@gmail.com>
2024-01-16 01:29:43 +0200

    yolo

--- src/main/java/victor/training/java/records/Records.java
+++ src/main/java/victor/training/java/records/Records.java
@@ -61,23 +61,3 @@ class Point {
     return x == point.x && y == point.y;
   }
 }
-
-
-// TODO 1 make Point a record
-// TODO 2 implement Point.translate(int deltaX, int deltaY):Point; Hint: you'll have to create a new instance
-//   eg new Point(1,2).translate(3,4).equals(new Point(4,6))
-// TODO 3 validate x and y < 1000 in constructor !!!
-// TODO 4 create overloaded constructor accepting x and y as strings! you will have to call this(int,int) aka "canonical" constructor
-// TODO 5 change the default toString to print itself in the format (x,y)
-
-// === Lessons: records is:
-// immutable
-// getters without "get" 🎉
-// hashcode/equals + toString
-// no extra fields besides signature
-// can have extra methods or statics
-// constructors: validation in default one + more overloaded ones
-// override generated methods
-// polymorphism: can implement, cannot extend
-// withers
-
