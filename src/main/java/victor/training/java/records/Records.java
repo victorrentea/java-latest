cdc4acbc7053aae960af8fe764b13b21ae53006f
Victor Rentea <victorrentea@gmail.com>
2024-02-07 15:23:39 +0200

    Lombok ftw .

--- src/main/java/victor/training/java/records/Records.java
+++ src/main/java/victor/training/java/records/Records.java
@@ -1,63 +1,28 @@
 package victor.training.java.records;
 
-import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.Objects;
+import lombok.Value;
+import lombok.With;
 
 public class Records {
   public static void main(String[] args) {
-    Point point = new Point();
-    point.setX(1);
-    point.setY(2);
+    Point point = new Point(1,2);
+//    point.setX(1);
+//    point.setY(2);
 
-    dark(point);
+    point = dark(point);
     System.out.println(point + " has x=" + point.getX());
   }
 
-  private static void dark(Point point) {
-    point.setX(-1);
+  private static Point dark(Point point){return point.withX(-1);
   }
 }
 
+//@Data  //hate
+@Value //love = @Data + all fields = private + final
+// Pro: better signal/noise ratio for code
+// Con: less control over the generated code, careless design
 class Point {
-  private int x;
-  private int y;
-
-  // behold: traditional Java boilerplate 🤢🤢🤢🤢
-  public int getX() {
-    return x;
-  }
-
-  public void setX(int x) {
-    this.x = x;
-  }
-
-  public int getY() {
-    return y;
-  }
-
-  public void setY(int y) {
-    this.y = y;
-  }
-
-  @Override
-  public String toString() {
-    return "Dot{x=" + x + ", y=" + y + '}';
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Point point = (Point) o;
-    return x == point.x && y == point.y;
-  }
+  @With
+  int x;
+  int y;
 }
