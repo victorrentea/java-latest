f5ac4231a172d48f0b56ec6bd5a7eaabc71aee0d
Victor Rentea <victorrentea@gmail.com>
2024-03-05 17:10:22 +0200

    Lombok

--- src/main/java/victor/training/java/records/Records.java
+++ src/main/java/victor/training/java/records/Records.java
@@ -1,65 +1,28 @@
 package victor.training.java.records;
 
-import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import lombok.Value;
+import lombok.With;
 
 import java.util.Objects;
 
 public class Records {
   public static void main(String[] args) {
-    Point point = new Point();
-    point.setX(1);
-    point.setY(2);
+    Point point = new Point(1,2);
 
-    dark(point);
-    System.out.println(point + " has x=" + point.getX());
+    Point point2 = execute(point);
+    System.out.println(point2 + " has x=" + point2.getX());
   }
 
-  private static void dark(Point point) {
-    point.setX(-1);
+  private static Point execute(Point point) {
+    return point.withX(-1);
   }
 }
 
+@Value
 class Point {
-  private int x;
-  private int y;
-
-  // behold: traditional Java boilerplate 🤢🤢🤢🤢
-  public int getX() {
-    return x;
-  }
-
-  public void setX(int x) {
-    this.x = x;
-  }
-
-  public int getY() {
-    return y;
-  }
-
-  public void setY(int y) {
-    this.y = y;
-  }
-
-  @Override
-  public String toString() {
-    return "Dot{x=" + x + ", y=" + y + '}';
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Point point = (Point) o;
-    return x == point.x && y == point.y;
-  }
+  @With
+  int x;
+  int y;
 }
 
 
