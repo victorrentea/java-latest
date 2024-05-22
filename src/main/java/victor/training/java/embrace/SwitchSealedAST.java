0a0f2821e243787eb60ba1762e3309fcab858097
Victor Rentea <victorrentea@gmail.com>
2024-05-22 07:40:15 +0300

    prep sealed expr

---
+++ src/main/java/victor/training/java/embrace/SwitchSealedAST.java
@@ -0,0 +1,19 @@
+package victor.training.java.embrace;
+
+public class SwitchSealedAST {
+  public static void main() {
+    // TODO 2 * 3 + 4 - 5
+    Expr e = null;
+
+    System.out.println(str(e) + " = " + eval(e));
+  }
+  static int eval(Expr e) {
+    return 0;
+  }
+  static String str(Expr e) {
+    return null;
+  }
+}
+interface Expr {
+}
+
