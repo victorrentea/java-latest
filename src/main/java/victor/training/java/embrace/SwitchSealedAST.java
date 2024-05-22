e1c86f2faaa065117d501bc5d8411a5a12a142a7
Victor Rentea <victorrentea@gmail.com>
2024-05-22 09:32:02 +0300

    prep

--- src/main/java/victor/training/java/embrace/SwitchSealedAST.java
+++ src/main/java/victor/training/java/embrace/SwitchSealedAST.java
@@ -4,8 +4,10 @@ import victor.training.java.embrace.Expr.*;
 
 public class SwitchSealedAST {
   public static void main() {
-    // TODO 2 * 3 + 4 - 5
-    Expr e = null;
+    // TODO 2 * 3 + 4
+    Expr e = new Sum(
+        new Prod(new Const(2), new Const(3)),
+        new Const(4));
 
     System.out.println(str(e) + " = " + eval(e));
   }
