84bd3e79c6e7c023a709f65bab5df77125a4fccd
Victor Rentea <victorrentea@gmail.com>
2024-05-22 09:21:20 +0300

    simplify

--- src/main/java/victor/training/java/embrace/SwitchSealed.java
+++ src/main/java/victor/training/java/embrace/SwitchSealed.java
@@ -11,8 +11,8 @@ record Square(int edge) implements Shape {
 
 void main(String[] args) {
   List<Shape> shapes = List.of(
-      new Square(10), // 4 * E
-      new Circle(5), // 2 * PI * R
+      new Square(10), // 4 e
+      new Circle(5), // 2 π r
       new Square(5));
 
   double totalPerimeter = 0; // TODO
