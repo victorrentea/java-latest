7819bd827e1d5c7bd4a5dad8852f53385b48423a
Victor Rentea <victorrentea@gmail.com>
2024-05-22 11:21:01 +0200

    compact

--- src/main/java/victor/training/java/embrace/SwitchSealed.java
+++ src/main/java/victor/training/java/embrace/SwitchSealed.java
@@ -2,36 +2,24 @@ package victor.training.java.embrace;
 
 import java.util.List;
 
-sealed interface Shape permits Circle, Square {
-}
-
-record Circle(int radius) implements Shape {
-
-}
-
-record Square(int edge) implements Shape {
-
-}
+sealed interface Shape permits Circle, Square { }
+record Circle(int radius) implements Shape { }
+record Square(int edge) implements Shape { }
 
 public class SwitchSealed {
-
   void main(String[] args) {
     List<Shape> shapes = List.of(
         new Square(10), // 4 edge
-        new Circle(5), // 2 π radius
-        new Square(5));
-
+        new Square(5),
+        new Circle(5)); // 2 π radius
 
     double totalPerimeter = 0; // TODO
-
     for (Shape shape : shapes) {
       // a. polymorphism / OOP
       // b. instanceOf
       // c. Visitor (anti)Pattern - 🤯 OMG
       // d. switch on sealed
-
     }
-
     System.out.println(totalPerimeter);
   }
 }
