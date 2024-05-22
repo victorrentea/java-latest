f9787edd25f9747f7f321f25da1235fc9dc40ca3
Victor Rentea <victorrentea@gmail.com>
2024-05-22 09:29:35 +0200

    prep

--- src/main/java/victor/training/java/embrace/ImmutableCollections.java
+++ src/main/java/victor/training/java/embrace/ImmutableCollections.java
@@ -4,13 +4,17 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 void main() { // stand-alone 'main' v21
+  //region avoid Arrays.asList
   List<Integer> list = Arrays.asList(1, 2, 3);
-  list.set(0, 3);
+  list.set(0, 3); // can change👎 elements
   System.out.println(list);
+  //endregion
 
+  //region avoid .collect(toList());
   var odds = list.stream()
       .filter(i -> i % 2 == 1)
-      .collect(toList());
+      .collect(toList()); // returns mutable👎 ArrayList
   odds.removeFirst();
   System.out.println(odds);
+  //endregion
 }
