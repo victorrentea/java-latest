51f81da90a0f895b819177e0c6545355095e85d8
Victor Rentea <victorrentea@gmail.com>
2024-07-11 09:55:09 +0300

    scoped vars + seq

---
+++ src/main/java/victor/training/java/virtualthread/scoped/ScopedValues.java
@@ -0,0 +1,38 @@
+package victor.training.java.virtualthread.scoped;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.StructuredTaskScope.ShutdownOnSuccess;
+
+import static java.util.concurrent.Executors.callable;
+
+@Slf4j
+public class ScopedValues {
+  public final static ScopedValue<String> scopedUser =
+      ScopedValue.newInstance();
+
+  public static void main() {
+    ScopedValue.where(scopedUser, "Victor").run(() -> {
+      method();
+      log.info("after={}", scopedUser.get());
+    });
+  }
+
+  @SneakyThrows
+  public static void method() {
+    log.info("in task={}", scopedUser.get());
+//    new Thread(ScopedValues::subtask).start();
+
+    try (var scope = new ShutdownOnSuccess()) {
+      scope.fork(callable(ScopedValues::subtask));
+      scope.fork(callable(ScopedValues::subtask));
+      scope.join();
+    }
+  }
+
+  public static void subtask() {
+    log.info("subtask={}", scopedUser.get());
+  }
+}
---
+++ src/main/java/victor/training/java/virtualthread/scoped/ThreadLocals.java
@@ -0,0 +1,25 @@
+package victor.training.java.virtualthread.scoped;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocals {
+  public final static ThreadLocal<String> threadLocalUser
+      = new ThreadLocal<>();
+
+  public static void main() {
+    threadLocalUser.set("Victor");
+    method();
+    log.info("after{}", threadLocalUser.get());
+  }
+
+  public static void method() {
+    log.info("in task={}", threadLocalUser.get());
+    new Thread(ThreadLocals::subtask).start();
+  }
+
+  public static void subtask() {
+    log.info("subtask={}", threadLocalUser.get());
+
+  }
+}
