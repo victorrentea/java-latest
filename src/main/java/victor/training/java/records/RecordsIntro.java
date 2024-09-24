d6bf7ce440b8598dc647d4f0dc66ad42e92c2ff5
Victor Rentea <victorrentea@gmail.com>
2024-09-24 14:06:53 +0300

    Lombok saved Java (of boilerplate)

--- src/main/java/victor/training/java/records/RecordsIntro.java
+++ src/main/java/victor/training/java/records/RecordsIntro.java
@@ -1,5 +1,7 @@
 package victor.training.java.records;
 
+import lombok.Data;
+
 import java.util.Objects;
 
 public class RecordsIntro {
@@ -11,43 +13,9 @@ public class RecordsIntro {
   }
 }
 
+@Data // =@Getter + @Setter + @ToString + @EqualsAndHashCode
 class Point {
   private int x;
   private int y;
-
-  // traditional Java boilerplate 🤢🤢🤢🤢
-  public int getX() {
-    return x;
-  }
-
-  public void setX(int x) {
-    this.x = x;
-  }
-
-  public int getY() {
-    return y;
-  }
-
-  public void setY(int y) {
-    this.y = y;
-  }
-
-  @Override
-  public String toString() {
-    return "Point{x=" + x + ", y=" + y + '}';
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Point point = (Point) o;
-    return x == point.x && y == point.y;
-  }
 }
 
