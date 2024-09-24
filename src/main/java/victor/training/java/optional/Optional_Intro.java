afda04e1a2a317f9b5ea6ceb591ed0d5ae8d8a1d
Victor Rentea <victorrentea@gmail.com>
2024-09-24 10:39:24 +0300

    Null Object Pattern + no NPE - the caller might not be aware of this convention
    Shines when the caller does not need to know about the NOOP convention

--- src/main/java/victor/training/java/optional/Optional_Intro.java
+++ src/main/java/victor/training/java/optional/Optional_Intro.java
@@ -5,10 +5,12 @@ package victor.training.java.optional;
 import victor.training.java.optional.model.Customer;
 import victor.training.java.optional.model.MemberCard;
 
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("ConstantConditions")
 public class Optional_Intro {
+
   // clothes shop
   public static void main(String[] args) {
     // test with 10 points or no MemberCard
@@ -31,10 +33,23 @@ public class Optional_Intro {
     if (card.getFidelityPoints() >= 50) {
       return new Discount(3, Map.of());
     }
-    return null;
+    return Discount.NO_DISCOUNT; // Null Object Pattern: a non-instance value that represents the absence of an object
   }
 
   public record Discount(int globalPercentage, Map<String, Integer> categoryDiscounts) {
+    public static final Discount NO_DISCOUNT = new Discount(0, Map.of());
   }
+
+  // Null object pattern:
+//    List<String> list = List.of(); // Null Object pattern for collection = natural null object.
+  //  for (String s : list) { .. }
+
+  // authorizer.authorize(action);
+  // Null object for Authorizer: Authorizer.NULL_AUTHORIZER.authorize(action);
+  // public static final Authorized NULL_AUTHORIZER =
+  //     new Authorizer() { @Override public void authorize(Action action) {/*nothing here*/} }
+  // inside the #authorize method does NO CHECK {}
+
+//    // NEVER EVER LEAVE A COLLECTION NULL => but []
 }
 
