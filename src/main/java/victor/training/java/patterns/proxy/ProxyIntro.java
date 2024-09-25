7a3eff6a65d8e552db1bc4ac0f67c68439207e95
Victor Rentea <victorrentea@gmail.com>
2024-09-25 11:16:05 +0300

    generated the subclass at runtime with a library = PROXY PATTERN in java.
    gave that proxy as a dependency to existing code to TRICK it to call ME and intercept their calls.

--- src/main/java/victor/training/java/patterns/proxy/ProxyIntro.java
+++ src/main/java/victor/training/java/patterns/proxy/ProxyIntro.java
@@ -3,6 +3,13 @@ package victor.training.java.patterns.proxy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ProxyIntro {
@@ -11,13 +18,22 @@ public class ProxyIntro {
     // TODO 1 : LOG the arguments of any invocation of a method in Maths w/ decorator
     // TODO 2 : without changing anything below the line (w/o any interface)
     // TODO 3 : so that any new methods in Maths are automatically logged [hard]
-    Maths maths = new Maths();
-    Maths decorated = new LoggingDecorator();
-//    Maths decoratedAgain = new TimingDecorator(decorated);
-    SecondGrade secondGrade = new SecondGrade(decorated);
-//    SecondGrade secondGrade = new SecondGrade(new TimingDecorator(new LoggingDecorator(new Maths())));
-//    SecondGrade secondGrade = new SecondGrade(new LoggingDecorator(new Maths()));
-//    SecondGrade secondGrade = new SecondGrade(new TimingDecorator(new Maths()));
+    Maths realMathsObject = new Maths();
+    Callback h = new MethodInterceptor() {
+      @Override
+      public Object intercept(Object obj, Method methodIntercepted, Object[] args, MethodProxy proxy) throws Throwable {
+        // this runs any method you call on the 'proxy' object below
+        // the method called is 'methodIntercepted'
+        System.out.println("Calling " + methodIntercepted.getName() + "(" + Arrays.toString(args) + ")");
+        return proxy.invoke(realMathsObject, args); // call the real method
+      }
+    };
+    Maths proxy = (Maths) Enhancer.create(Maths.class, h); // instace of a subclass generated at runtime
+    SecondGrade secondGrade = new SecondGrade(proxy);
+
+    // java calls an method interceptor a 'proxy' if it's generated at runtime
+//    Maths decorated = new LoggingDecorator(maths);
+//    SecondGrade secondGrade = new SecondGrade(proxy);
     new ProxyIntro().run(secondGrade);
     // TODO 4 : let Spring do its job, and do the same with an Aspect
 //         SpringApplication.run(ProxyIntro.class, args);
@@ -37,20 +53,19 @@ public class ProxyIntro {
 // also measure how much time each method takes
 // REQUIREMENT: any method in Maths class should log its arguments
 // without changing the Maths class. use OOP
-@RequiredArgsConstructor
-class LoggingDecorator extends Maths {
+// frameworks can generate this class at runtime for us: example: CGLIB, ByteBuddy, Javassist, ASM, etc.
+//@RequiredArgsConstructor
+//class LoggingDecorator extends Maths {
 //  private final Maths decorated;
-  public int sum(int a, int b) {
-    System.out.println("sum(" + a + "," + b + ")");
+//  public int sum(int a, int b) {
+//    System.out.println("sum(" + a + "," + b + ")");
 //    return decorated.sum(a, b);
-    return super.sum(a, b);
-  }
-  public int product(int a, int b) {
-    System.out.println("product(" + a + "," + b + ")");
+//  }
+//  public int product(int a, int b) {
+//    System.out.println("product(" + a + "," + b + ")");
 //    return decorated.product(a, b);
-    return super.sum(a, b);
-  }
-}
+//  }
+//}
 
 //  Maths maths = new Maths();
 //    LoggingDecorator decorator = new LoggingDecorator(maths);
@@ -67,6 +82,9 @@ class SecondGrade {
     System.out.println("2x3=" + maths.product(2, 3));
   }
 }
+
+// Thoughts:
+// - HOW about private methods? They are not inherited, so they are not intercepted
 
 class Maths {
   public int sum(int a, int b) {
