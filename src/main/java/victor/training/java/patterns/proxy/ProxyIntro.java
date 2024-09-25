3cf9695fad4f8d0ab5b91b79a96919988a41d719
Victor Rentea <victorrentea@gmail.com>
2024-09-25 09:58:42 +0300

    add code for patterns in modern java

---
+++ src/main/java/victor/training/java/patterns/proxy/ProxyIntro.java
@@ -0,0 +1,72 @@
+package victor.training.java.patterns.proxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class ProxyIntro {
+    public static void main(String[] args) {
+        // Play the role of Spring here (there's no framework)
+        // TODO 1 : LOG the arguments of any invocation of a method in Maths w/ decorator
+        // TODO 2 : without changing anything below the line (w/o any interface)
+        // TODO 3 : so that any new methods in Maths are automatically logged [hard]
+
+        Maths maths = new Maths();
+
+        SecondGrade secondGrade = new SecondGrade(maths);
+
+        new ProxyIntro().run(secondGrade);
+
+        // TODO 4 : let Spring do its job, and do the same with an Aspect
+         SpringApplication.run(ProxyIntro.class, args);
+    }
+
+    // =============== THE LINE =================
+
+    @Autowired
+    public void run(SecondGrade secondGrade) {
+        System.out.println("At runtime...");
+        secondGrade.mathClass();
+    }
+
+}
+
+@Service
+class SecondGrade {
+    private final Maths maths;
+
+    SecondGrade(Maths maths) {
+        this.maths = maths;
+    }
+
+    public void mathClass() {
+        System.out.println("2+4=" + maths.sum(2, 4));
+        System.out.println("1+5=" + maths.sum(1, 5));
+        System.out.println("2x3=" + maths.product(2, 3));
+    }
+}
+
+@Service
+class Maths {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int product(int a, int b) {
+        int total = 0;
+        for (int i = 0; i < a; i++) {
+            total = sum(total, b);
+        }
+        return total;
+    }
+}
+
+
+// Key Points
+// [2] Class Proxy using CGLIB (Enhancer) extending the proxied class
+// [3] Spring Cache support [opt: redis]
+// [4] Custom @Aspect, applied to methods in @Facade
+// [6] Tips: self proxy, debugging, final
+// [7] OPT: Manual proxying using BeanPostProcessor
---
+++ src/main/java/victor/training/java/patterns/strategy/StrategySpringApp.java
@@ -0,0 +1,30 @@
+package victor.training.java.patterns.strategy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class StrategySpringApp implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(StrategySpringApp.class, args);
+    }
+
+    @Autowired
+    private CustomsService service;
+
+    // TODO [1] Break CustomsService logic into Strategies
+    // TODO [2] Convert it to Chain Of Responsibility
+    // TODO [3] Wire with Spring
+    public void run(String... args) {
+        System.out.println("Tax for (RO,100,100) = " + service.calculateCustomsTax(
+                new Parcel("RO", 100, 100, LocalDate.now())));
+        System.out.println("Tax for (CN,100,100) = " + service.calculateCustomsTax(
+                new Parcel("CN", 100, 100, LocalDate.now())));
+        System.out.println("Tax for (UK,100,100) = " + service.calculateCustomsTax(
+                new Parcel("UK", 100, 100, LocalDate.now())));
+    }
+}
