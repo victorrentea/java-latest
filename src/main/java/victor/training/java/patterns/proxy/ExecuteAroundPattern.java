6b4eb27b302c96d8b56f0e75e2ffc26b5f5b3cc2
Victor Rentea <victorrentea@gmail.com>
2024-09-25 12:03:29 +0300

    FP alternative to AOP: passing the code to execute in a given context (t0/t1)

---
+++ src/main/java/victor/training/java/patterns/proxy/ExecuteAroundPattern.java
@@ -0,0 +1,45 @@
+package victor.training.java.patterns.proxy;
+
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.ThreadUtils;
+
+import java.util.function.Supplier;
+
+import static java.time.Duration.ofSeconds;
+
+public class ExecuteAroundPattern {
+  public static void main(String[] args) {
+    int result = measureTime(  ()   -> sum(2,3)     );
+    System.out.println("Computed: " + result);
+
+    int multiplyResult = measureTime(  ()   -> multiply(2,3)     );
+    System.out.println("Computed: " + multiplyResult);
+
+    // private final MeterRegistry meterRegistry; // micrometer = standard in Java to report metrics
+    //
+    //  public void method() {
+    //    log.info("Doing something");
+    //    meterRegistry.timer("my.timer").record(() -> {
+    //      log.info("Doing something expensive");
+    //    });
+    //  }
+  }
+
+  public static <T> T measureTime(Supplier<T> codeToMeasure) {
+    long t0 = System.currentTimeMillis();
+    T r = codeToMeasure.get();
+    long t1 = System.currentTimeMillis();
+    System.out.println("Took " + (t1 - t0) + "ms");
+    return r;
+  }
+
+
+  @SneakyThrows
+  public static int sum(int a, int b) {
+    ThreadUtils.sleep(ofSeconds(1));
+    return a + b;
+  }
+  public static int multiply(int a, int b) {
+    return a * b;
+  }
+}
