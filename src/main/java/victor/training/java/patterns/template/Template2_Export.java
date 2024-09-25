a964d5a6965407f3f9758693da43e2f679a11f7a
Victor Rentea <victorrentea@gmail.com>
2024-09-25 14:50:57 +0300

    I've extended a concrete class and I @Override-d one of its concrete methods
    NEVER DO THIS!
    PR rejected.

--- src/main/java/victor/training/java/patterns/template/Template2_Export.java
+++ src/main/java/victor/training/java/patterns/template/Template2_Export.java
@@ -3,9 +3,12 @@ package victor.training.java.patterns.template;
 import lombok.RequiredArgsConstructor;
 import victor.training.java.patterns.template.support.Order;
 import victor.training.java.patterns.template.support.OrderRepo;
+import victor.training.java.patterns.template.support.Product;
+import victor.training.java.patterns.template.support.ProductRepo;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.util.Objects;
 
@@ -14,12 +17,13 @@ public class Template2_Export {
   private final FileExporter exporter;
 
   public void exportOrders() throws Exception {
-    exporter.exportOrders();
+    exporter.exportOrders("orders.csv");
   }
 
   public void exportProducts() throws Exception {
     // TODO 'the same way you did the export of orders'
     // RUN UNIT TESTS!
+    exporter.exportOrders("products.csv");
   }
 }
 
@@ -29,17 +33,13 @@ class FileExporter {
   private final OrderRepo orderRepo;
   private final File exportFolder;
 
-  public File exportOrders() {
-    File file = new File(exportFolder, "orders.csv");
+  public File exportOrders(String fileName) {
+    File file = new File(exportFolder, fileName);
     long t0 = System.currentTimeMillis();
     try (Writer writer = new FileWriter(file)) { // java 7 try-with-resources
       System.out.println("Starting export to: " + file.getAbsolutePath());
 
-      writer.write("OrderID;CustomerId;Amount\n"); // header
-      for (Order order : orderRepo.findByActiveTrue()) {// body
-        String csv = order.id() + ";" + order.customerId() + ";" + order.amount() + "\n";
-        writer.write(csv);
-      }
+      writeContents(writer);
 
       System.out.println("File export completed: " + file.getAbsolutePath());
       return file;
@@ -52,12 +52,36 @@ class FileExporter {
     }
   }
 
+  protected void writeContents(Writer writer) throws IOException {
+    writer.write("OrderID;CustomerId;Amount\n"); // header
+    for (Order order : orderRepo.findByActiveTrue()) {// body
+      String csv = order.id() + ";" + order.customerId() + ";" + order.amount() + "\n";
+      writer.write(csv);
+    }
+  }
+
   public String escapeCell(Object cellValue) {
     if (cellValue instanceof String s) {
       if (!s.contains("\n")) return s;
       return "\"" + s.replace("\"", "\"\"") + "\"";
     } else {
       return Objects.toString(cellValue);
+    }
+  }
+}
+
+class ProductExporter extends FileExporter {
+  private final ProductRepo productRepo;
+  public ProductExporter(OrderRepo orderRepo, File exportFolder, ProductRepo productRepo) {
+    super(orderRepo, exportFolder);
+    this.productRepo = productRepo;
+  }
+
+  protected void writeContents(Writer writer) throws IOException {
+    writer.write("ProductID;Name;Price\n"); // header
+    for (Product product : productRepo.findAll()) {
+      String csv = product.id() + ";" + product.name() + ";" + product.price() + "\n";
+      writer.write(csv);
     }
   }
 }
