f2be9d8624f0920d128f8035fa627bcb2b971d67
Victor Rentea <victorrentea@gmail.com>
2024-09-25 14:32:11 +0300

    ready

--- src/main/java/victor/training/java/patterns/template/Template2_Export.java
+++ src/main/java/victor/training/java/patterns/template/Template2_Export.java
@@ -11,57 +11,53 @@ import java.util.Objects;
 
 @RequiredArgsConstructor
 public class Template2_Export {
-    private final FileExporter exporter;
+  private final FileExporter exporter;
 
-    public void exportOrders() throws Exception {
-        exporter.exportOrders();
-    }
+  public void exportOrders() throws Exception {
+    exporter.exportOrders();
+  }
 
-    public void exportProducts() throws Exception {
-        // TODO 'the same way you did the export of orders'
-        // RUN UNIT TESTS!
-    }
+  public void exportProducts() throws Exception {
+    // TODO 'the same way you did the export of orders'
+    // RUN UNIT TESTS!
+  }
 }
 
 
+@RequiredArgsConstructor
 class FileExporter {
-    private final OrderRepo orderRepo;
-    private final File exportFolder;
+  private final OrderRepo orderRepo;
+  private final File exportFolder;
 
-    public FileExporter(OrderRepo orderRepo, File exportFolder) {
-        this.orderRepo = orderRepo;
-        this.exportFolder = exportFolder;
+  public File exportOrders() {
+    File file = new File(exportFolder, "orders.csv");
+    long t0 = System.currentTimeMillis();
+    try (Writer writer = new FileWriter(file)) {
+      System.out.println("Starting export to: " + file.getAbsolutePath());
+
+      writer.write("OrderID;Date\n");
+
+      for (Order order : orderRepo.findByActiveTrue()) {
+        String csv = order.id() + ";" + order.customerId() + ";" + order.amount() + "\n";
+        writer.write(csv);
+      }
+
+      System.out.println("File export completed: " + file.getAbsolutePath());
+      return file;
+    } catch (Exception e) {
+      System.out.println("Pretend: Send Error Notification Email"); // TODO CR: only for export orders, not for products
+      throw new RuntimeException("Error exporting data", e);
+    } finally {
+      System.out.println("Pretend: Metrics: Export finished in: " + (System.currentTimeMillis() - t0));
     }
+  }
 
-    public File exportOrders() {
-        File file = new File(exportFolder, "orders.csv");
-        long t0 = System.currentTimeMillis();
-        try (Writer writer = new FileWriter(file)) {
-            System.out.println("Starting export to: " + file.getAbsolutePath());
-
-            writer.write("OrderID;Date\n");
-
-            for (Order order : orderRepo.findByActiveTrue()) {
-                String csv = order.id() + ";" + order.customerId() + ";" + order.amount() + "\n";
-                writer.write(csv);
-            }
-
-            System.out.println("File export completed: " + file.getAbsolutePath());
-            return file;
-        } catch (Exception e) {
-            System.out.println("Pretend: Send Error Notification Email"); // TODO CR: only for export orders, not for products
-            throw new RuntimeException("Error exporting data", e);
-        } finally {
-            System.out.println("Pretend: Metrics: Export finished in: " + (System.currentTimeMillis() - t0));
-        }
+  public String escapeCell(Object cellValue) {
+    if (cellValue instanceof String s) {
+      if (!s.contains("\n")) return s;
+      return "\"" + s.replace("\"", "\"\"") + "\"";
+    } else {
+      return Objects.toString(cellValue);
     }
-
-    public String escapeCell(Object cellValue) {
-        if (cellValue instanceof String s) {
-            if (!s.contains("\n")) return s;
-            return "\"" + s.replace("\"", "\"\"") + "\"";
-        } else {
-            return Objects.toString(cellValue);
-        }
-    }
+  }
 }
