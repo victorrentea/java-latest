1026fac4be9fde7b50daeda1397e8e2a8e8035ee
Victor Rentea <victorrentea@gmail.com>
2024-09-26 10:08:26 +0300

    cf

---
+++ src/main/java/victor/training/java/cf/Beer.java
@@ -0,0 +1,8 @@
+package victor.training.java.cf;
+
+import lombok.Data;
+
+@Data
+public class Beer {
+   private String type;
+}
---
+++ src/main/java/victor/training/java/cf/Vodka.java
@@ -0,0 +1,8 @@
+package victor.training.java.cf;
+
+import lombok.Data;
+
+@Data
+public class Vodka {
+  private String brand;
+}
