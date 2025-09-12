260ee00ea44eb02916e6570369f7be0397186476
Victor Rentea <victorrentea@gmail.com>
2025-09-12 14:32:00 +0300

    java8+: Loan pattern: lasi clientii sa lucreze cu fisierul mangeuit de tine, CsvExporter.

--- src/main/java/victor/training/java/patterns/template/Template2_Export.java
+++ src/main/java/victor/training/java/patterns/template/Template2_Export.java
@@ -1,6 +1,7 @@
 package victor.training.java.patterns.template;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.function.ThrowingConsumer;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,18 +12,29 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class Template2_Export {
     private final FileExporter exporter;
+    private final OrderRepo orderRepo;
 
     public void exportOrders() throws Exception {
-        exporter.exportOrders();
+        // loan pattern: acepti param un Consumer<Resursa> mangeuita de tine
+        exporter.exportCsv(writer -> {
+            writer.write("OrderID;CustomerId;Amount\n");
+            for (Order order : orderRepo.findByActiveTrue()) {
+                String csv = order.id() + ";" + order.customerId() + ";" + order.amount() + "\n";
+                writer.write(csv);
+            }
+        });
     }
-
+    private final ProductRepo productRepo;
     public void exportProducts() throws Exception {
-        // TODO 'the same way you did the export of orders'
-        // RUN UNIT TESTS!
+        exporter.exportCsv(writer -> {
+            writer.write("ProductID;Name;Price;ImageUrl\n");
+            for (Product product : productRepo.findAll()) {
+                writer.write(product.id() + ";" + product.name() + ";" + product.price() + ";" +
+                             exporter.escapeCell(product.imageUrl()) + "\n");
+            }
+        });
     }
 }
-
-
 class FileExporter {
     private final OrderRepo orderRepo;
     private final File exportFolder;
@@ -32,18 +44,17 @@ class FileExporter {
         this.exportFolder = exportFolder;
     }
 
-    public File exportOrders() {
+    // f(header:string, Enum<?> columns, List<T> date); enum OrderColumns{ID("OrderId", Order::getId),..}
+
+
+
+    public File exportCsv(ThrowingConsumer<Writer> throwingConsumer) {
         File file = new File(exportFolder, "orders.csv");
         long t0 = System.currentTimeMillis();
         try (Writer writer = new FileWriter(file)) {
             System.out.println("Starting export to: " + file.getAbsolutePath());
 
-            writer.write("OrderID;Date\n");
-
-            for (Order order : orderRepo.findByActiveTrue()) {
-                String csv = order.id() + ";" + order.customerId() + ";" + order.amount() + "\n";
-                writer.write(csv);
-            }
+            throwingConsumer.acceptWithException(writer);
 
             System.out.println("File export completed: " + file.getAbsolutePath());
             return file;
@@ -67,7 +78,7 @@ class FileExporter {
 record Order(Long id, Long customerId, Double amount) {
 }
 interface OrderRepo {
-    List<Order> findByActiveTrue(); // 1 Mln orders ;)
+    Iterable<Order> findByActiveTrue(); // 1 Mln orders ;)
 }
 
 interface ProductRepo {
